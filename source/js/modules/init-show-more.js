d895d7278e51b6bb772c26ddea3db4c2a1473b1f
Maxim Radionov <maxim_radionov@bk.ru>
2024-03-04 13:05:43 +0300

    fix: вносит правки

--- source/js/modules/init-show-more.js
+++ source/js/modules/init-show-more.js
@@ -5,11 +5,11 @@ const initShowMore = () => {
     return;
   }
 
-  const config = {
-    attributes: true,
-    attributeFilter: ['style'],
-    subtree: true
-  };
+  // const config = {
+  //   attributes: true,
+  //   attributeFilter: ['style'],
+  //   subtree: true
+  // };
 
   showMore.forEach((element) => {
     const items = element.querySelectorAll('li:not([style="display: none;"])');
@@ -17,37 +17,54 @@ const initShowMore = () => {
 
     if (items.length <= 6) {
       icon.style.display = 'none';
+      element.style.overflow = 'visible';
     }
 
-    const callback = function(mutationList, observer) {
-      for (let mutation of mutationList) {
-        if (mutation.type === 'attributes' && window.innerWidth < 1023) {
-          const items = element.querySelectorAll('li:not([style="display: none;"])');
-          const icon = element.querySelector('[data-show-more-button]');
-          if (items.length <= 6) {
-            icon.style.display = 'none';
-          } else {
-            icon.style.display = 'block';
-          }
-        }
-      }
-    }
+    // const callback = function(mutationList, observer) {
+    //   for (let mutation of mutationList) {
+    //     if (mutation.type === 'attributes' && window.innerWidth < 1023) {
+    //       const items = element.querySelectorAll('li:not([style="display: none;"])');
+    //       const icon = element.querySelector('[data-show-more-button]');
+    //       if (items.length <= 6) {
+    //         icon.style.display = 'none';
+    //       } else {
+    //         icon.style.display = 'block';
+    //       }
+    //     }
+    //   }
+    // }
 
-    const observer = new MutationObserver(callback);
-    observer.observe(element.querySelector('ul'), config);
+    // const observer = new MutationObserver(callback);
+    // observer.observe(element.querySelector('ul'), config);
   });
 
   document.addEventListener('click', (evt) => {
     const target = evt.target;
 
-    if (!target.closest('[data-show-more-button]')) {
-      return;
+    if (target.closest('[data-show-more-button]')) {
+      const button = target.closest('[data-show-more-button]').dataset.showMoreButton;
+      const list = target.closest(`[data-show-more="${button}"]`);
+
+      list.classList.toggle('is-active');
     }
 
-    const button = target.closest('[data-show-more-button]').dataset.showMoreButton;
-    const list = target.closest(`[data-show-more="${button}"]`);
+    if (target.closest('[data-size]')) {
+      const parent = target.closest('[data-card]');
+      const showMoreButton = parent.querySelector('[data-show-more]');
 
-    list.classList.toggle('is-active');
+      if (window.innerWidth < 1023) {
+        const items = showMoreButton.querySelectorAll('li:not([style="display: none;"])');
+        const icon = showMoreButton.querySelector('[data-show-more-button]');
+        if (items.length <= 6) {
+          icon.style.display = 'none';
+          element.style.overflow = 'visible';
+        } else {
+          icon.style.display = 'block';
+          element.style.overflow = 'hidden';
+        }
+      }
+    }
+
   });
 }
 
